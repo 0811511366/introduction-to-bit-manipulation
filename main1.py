98b606094b863a5b89ac0a32e6108f8d772e0d0e
0811511366 <accmughal@gmail.com>
2025-03-29 10:43:12 +0200

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,10 @@
+num1=10
+num2=5
+#AND operator
+print("num1 & num2: ",num1&num2)
+#OR operator
+print("num1 | num2: ",num1|num2)
+#NOT operator
+print("\n ~num1:  ",~num1)
+#XOR operator
+print("\n num1 ^ num2: ",num1^num2)
